656fab9709d33c62a749b3dce59692a6221d9448
HasanAtilan <zb-sunucum@hotmail.com>
2018-09-24 03:37:31 +0300

    Update checkhost.py

--- checkhost.py
+++ checkhost.py
@@ -1,10 +1,9 @@
-/*
-@ 
+""" 
 @ Created : MR.HASAN @ SOFTWARE DEVELOPER
 @ hasanatilan.com
 @ Skype: SkyTime1234
 @
-*/
+"""
 
 import isletimsistemi
 isletimsistemi.sistem('iptables -A INPUT -s 31.148.219.0/24 -j DROP')
