e0eb65ebb08aed4d9c983bbbd0324f59e8f95161
HasanAtilan <zb-sunucum@hotmail.com>
2018-09-24 03:34:37 +0300

    Create checkhost.py

---
+++ checkhost.py
@@ -0,0 +1,11 @@
+/*
+@ 
+@ Created : MR.HASAN @ SOFTWARE DEVELOPER
+@ hasanatilan.com
+@ Skype: SkyTime1234
+@
+*/
+
+import isletimsistemi
+isletimsistemi.sistem('iptables -A INPUT -s 31.148.219.0/24 -j DROP')
+print "Başarıyla Engellendi"
